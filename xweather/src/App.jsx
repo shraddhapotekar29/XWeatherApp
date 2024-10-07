f5b6bca0963db96ed6bc564347bd867b87a6dfd9
shraddha potekar <shraddhapotekar762@gmail.com>
2024-10-07 15:27:58 +0530

    weather app

---
+++ xweather/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import WeatherApp from './Weather/Weather' 
+function App() {
+  
+  return (
+    <div style={{backgroundColor:"#87CEEB",height:"100vh"}}>
+<WeatherApp/>
+    </div>
+  )
+}
+
+export default App
