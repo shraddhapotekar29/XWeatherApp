aeb70dcce15a7a13702c25043ce5f784fbfbd428
shraddha potekar <shraddhapotekar762@gmail.com>
2024-10-10 15:08:35 +0530

    weather app

--- xweather/src/Weather/Weather.jsx
+++ xweather/src/Weather/Weather.jsx
@@ -38,7 +38,10 @@ const WeatherApp=()=>{
     <input type="text" placeholder="Enter City Name" style={{padding:"0.5rem",marginRight:"0.5rem"}} onChange={(e)=>setCityName(e.target.value)} />
     <button style={{backgroundColor:"green",padding:"0.5rem"}} type="submit">Search</button>
     </form>
+
+
     {isLoading && <p style={{textAlign:"center"}}>Loading data...</p>}
+
     {isCityPresent && city && <div className="weather-cards" style={{display:"flex",alignItems:"center",justifyContent:"center",marginTop:"2rem"}}>
     <Grid container>
     <Grid item xs={12} sm={6} md={3} >
@@ -47,19 +50,19 @@ const WeatherApp=()=>{
         <p>{city.current.temp_c}°C</p>
         </div>
         </Grid>
-        <Grid item className="weather-card" xs={12} sm={6} md={3}>
+        <Grid item xs={12} sm={6} md={3}>
         <div className="weather-card"  style={{backgroundColor:"#ffffff",textAlign:"center",margin:"1rem",width:"20rem",padding:"1rem",borderRadius:"8px"}}>
         <h4>Humidity</h4>
         <p>{city.current.humidity}%</p>
         </div>
         </Grid>
-        <Grid item className="weather-card" xs={12} sm={6} md={3}>
+        <Grid item xs={12} sm={6} md={3}>
         <div className="weather-card"  style={{backgroundColor:"#ffffff",textAlign:"center",margin:"1rem",width:"20rem",padding:"1rem",borderRadius:"8px"}}>
         <h4>Condition</h4>
         <p>{city.current.condition.text}</p>
         </div>
         </Grid>
-        <Grid item className="weather-card" xs={12} sm={6} md={3}>
+        <Grid item xs={12} sm={6} md={3}>
         <div className="weather-card"  style={{backgroundColor:"#ffffff",margin:"1rem",width:"20rem",padding:"1rem",textAlign:"center",borderRadius:"8px"}}>
         <h4>Wind Speed</h4>
         <p>{city.current.wind_kph} kph</p>
